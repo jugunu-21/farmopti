a0fa26d3867fd071cfc6093e928aca83b71b8da4
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-01 13:06:13 +0530

    redefining the  navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,14 +8,14 @@ import useMeasure from "react-use-measure";
 const Example = () => {
   return (
     <section
-      className="relative h-[150vh] w-full overflow-hidden bg-black"
-      style={{
-        backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23171717'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
-      }}
+      className="relative h-[8vh] w-full overflow-hidden "
+      // style={{
+      //   backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23171717'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
+      // }}
     >
       <GlassNavigation />
 
-      <span className="absolute -top-[600px] left-[50%] h-[800px] w-4/5 max-w-3xl -translate-x-[50%] rounded bg-neutral-900" />
+      {/* <span className="absolute -top-[600px] left-[50%] h-[800px] w-4/5 max-w-3xl -translate-x-[50%] rounded bg-neutral-900" /> */}
     </section>
   );
 };
@@ -57,7 +57,7 @@ const GlassNavigation = () => {
       style={{
         cursor: hovered ? "none" : "auto",
       }}
-      className="glass-nav fixed left-0 right-0 top-0 z-10 mx-auto max-w-6xl overflow-hidden border-[1px] border-white/10 bg-gradient-to-br from-white/20 to-white/5 backdrop-blur md:left-6 md:right-6 md:top-6 md:rounded-2xl"
+      className="glass-nav fixed left-0 right-0 top-0 z-10 mx-auto max-w-6xl overflow-hidden border-[1px] border-black/10  from-black/20 to-black/5 backdrop-blur md:left-6 md:right-6 md:top-6 md:rounded-2xl"
     >
       <div className="glass-nav flex items-center justify-between px-5 py-5">
         <Cursor hovered={hovered} scope={scope} />
@@ -94,13 +94,13 @@ const Cursor = ({
       ref={scope}
       className="pointer-events-none absolute z-0 grid h-[50px] w-[50px] origin-[0px_0px] place-content-center rounded-full bg-gradient-to-br from-indigo-600 from-40% to-indigo-400 text-2xl"
     >
-      <FiArrowUpRight className="text-white" />
+      <FiArrowUpRight className="text-black" />
     </motion.span>
   );
 };
 
 const Logo = () => (
-  <span className="pointer-events-none relative left-0 top-[50%] z-10 text-4xl font-black text-white mix-blend-overlay md:absolute md:left-[50%] md:-translate-x-[50%] md:-translate-y-[50%]">
+  <span className="pointer-events-none relative left-0 top-[50%] z-10 text-4xl font-black text-black mix-blend-overlay md:absolute md:left-[50%] md:-translate-x-[50%] md:-translate-y-[50%]">
     logo.
   </span>
 );
@@ -119,17 +119,17 @@ const GlassLink = ({ text }: { text: string }) => {
       href="#"
       className="group relative scale-100 overflow-hidden rounded-lg px-4 py-2 transition-transform hover:scale-105 active:scale-95"
     >
-      <span className="relative z-10 text-white/90 transition-colors group-hover:text-white">
+      <span className="relative z-10 text-black/90 transition-colors group-hover:text-black">
         {text}
       </span>
-      <span className="absolute inset-0 z-0 bg-gradient-to-br from-white/20 to-white/5 opacity-0 transition-opacity group-hover:opacity-100" />
+      <span className="absolute inset-0 z-0 bg-gradient-to-br from-black/20 to-black/5 opacity-0 transition-opacity group-hover:opacity-100" />
     </a>
   );
 };
 
 const TextLink = ({ text }: { text: string }) => {
   return (
-    <a href="#" className="text-white/90 transition-colors hover:text-white">
+    <a href="#" className="text-black/90 transition-colors hover:text-black">
       {text}
     </a>
   );
@@ -145,13 +145,13 @@ const Buttons = ({
       <SignInButton />
     </div>
 
-    <button className="relative scale-100 overflow-hidden rounded-lg bg-gradient-to-br from-indigo-600 from-40% to-indigo-400 px-4 py-2 font-medium text-white transition-transform hover:scale-105 active:scale-95">
+    <button className="relative scale-100 overflow-hidden rounded-lg bg-gradient-to-br from-indigo-600 from-40% to-indigo-400 px-4 py-2 font-medium text-black transition-transform hover:scale-105 active:scale-95">
       Try free
     </button>
 
     <button
       onClick={() => setMenuOpen((pv) => !pv)}
-      className="ml-2 block scale-100 text-3xl text-white/90 transition-all hover:scale-105 hover:text-white active:scale-95 md:hidden"
+      className="ml-2 block scale-100 text-3xl text-black/90 transition-all hover:scale-105 hover:text-black active:scale-95 md:hidden"
     >
       <FiMenu />
     </button>
@@ -161,10 +161,10 @@ const Buttons = ({
 const SignInButton = () => {
   return (
     <button className="group relative scale-100 overflow-hidden rounded-lg px-4 py-2 transition-transform hover:scale-105 active:scale-95">
-      <span className="relative z-10 text-white/90 transition-colors group-hover:text-white">
+      <span className="relative z-10 text-black/90 transition-colors group-hover:text-black">
         Sign in
       </span>
-      <span className="absolute inset-0 z-0 bg-gradient-to-br from-white/20 to-white/5 opacity-0 transition-opacity group-hover:opacity-100" />
+      <span className="absolute inset-0 z-0 bg-gradient-to-br from-black/20 to-black/5 opacity-0 transition-opacity group-hover:opacity-100" />
     </button>
   );
 };
