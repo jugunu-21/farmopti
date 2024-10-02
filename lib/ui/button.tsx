a6b3100e71ffedb421907f3bb9a9becdc4c982b6
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-02 22:46:54 +0530

    add navigation with id

--- lib/ui/button.tsx
+++ lib/ui/button.tsx
@@ -3,7 +3,7 @@ export default function Gradientbutton (  {text,cta,className}:{text:string,cta:
   const router=useRouter()
     return(
     <button   onClick={() => router.push(cta || '/')} 
-    className={`relative scale-100 overflow-hidden rounded-lg bg-gradient-to-br from-amber-600 from-40% to-amber-400 px-4 py-2 font-medium text-black transition-transform hover:scale-105 active:scale-95 ${className}`}>
+    className={`relative scale-100 overflow-hidden rounded-lg bg-green-300 px-4 py-2 font-medium text-black transition-transform hover:scale-105 active:scale-95 ${className}`}>
     {text}
   </button>
 
