d7d04732bbfa632643ade074462feb1aca8efe8f
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-03 01:49:47 +0530

    chnage in font

--- components/contact.tsx
+++ components/contact.tsx
@@ -19,12 +19,12 @@ export default function Contact() {
       </div>
       <div className="container px-5 py-24 mx-auto flex">
         <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Get in Touch</h2>
+          <h2 className="text-gray-900 text-xl font-bold mb-1  title-font">Get in Touch</h2>
           <p className="leading-relaxed mb-5 text-gray-600">
             Have questions about how our AI-driven platform can help optimize your farm's yield,
           </p>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            <label htmlFor="email" className="leading-7 text-lg font-semibold text-gray-600">
               Your Email
             </label>
             <input
@@ -35,7 +35,7 @@ export default function Contact() {
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="message" className="leading-7 text-sm text-gray-600">
+            <label htmlFor="message" className="leading-7 text-lg font-semibold text-gray-600">
               How can we assist you?
             </label>
             <textarea
