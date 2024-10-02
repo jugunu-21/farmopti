eec2f3ab5cd2a544f51d0bc8714ad3098004de26
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-02 07:13:02 +0530

    add contact page

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -1,9 +1,10 @@
-import Contact from "@/components/contact"
+"use client"
+import {SlideInAuth} from "@/components/comonent"
 
 export default function ContactDashboard(){
     return(
         <>
-        <Contact/>
+        <SlideInAuth/>
         </>
     )
 }
