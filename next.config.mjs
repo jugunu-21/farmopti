caeca871600e479a3cc4891576465f509f19fef7
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-03 15:40:27 +0530

    adding image domain in next config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// // // Assuming you want to add the images configuration to nextConfig
+// // Object.assign(nextConfig, {
+// //   images: {
+// //     domains: ['example.com', 'anotherdomain.com', 'aceternity.com','www.shutterstock.com'],
+// //   },
+// // });
+
+// export default nextConfig;
+const nextConfig = {
+    experimental: {
+      fontOptimization: false,
+    },
+    images: {
+      domains: ['example.com', 'anotherdomain.com', 'aceternity.com','www.shutterstock.com','c7.alamy.com','api.dicebear.com',"unsplash.com/images",
+        "https://www.pexels.com/search/free%20download/"
+      ],
+    },
+    // Existing configuration...
+  };
+  
+  export default nextConfig;
